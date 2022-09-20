78b8748f0c2ecfe538a0452df1df05b3c7c02234
Joshua Kennedy <joshkennede@gmail.com>
2022-09-19 17:30:24 -0700

    Added logic to service methods

--- Solver.Service/SolverService.cs
+++ Solver.Service/SolverService.cs
@@ -1,10 +1,35 @@
-﻿namespace Solver.Service
+﻿using CsvHelper;
+using CsvHelper.Configuration;
+using System.Globalization;
+using Solver.Repository;
+
+namespace Solver.Service
 {
     public class SolverService : ISolverService
     {
-        public Task<(bool, string)> GenerateCSV(IEnumerable<object> results, string fileNameAndPath)
+        private readonly ISolverRepository repository;
+
+        public SolverService(ISolverRepository repository)
         {
-            throw new NotImplementedException();
+            this.repository = repository;
+        }
+
+        public async Task<IEnumerable<IDictionary<string, object>>?> GetDataAsDictionary(string databaseId, string schema, string tableName)
+        {
+            var data = await repository.GetDataAsDictionary(databaseId, schema, tableName);
+            if (data != null)
+                return data;
+            else
+                return null;
+        }
+
+        public async Task<IEnumerable<object>?> GetDataAsObjects(string databaseId, string schema, string tableName)
+        {
+            var data = await repository.GetDataAsObjects(databaseId, schema, tableName);
+            if (data != null)
+                return data;
+            else
+                return null;
         }
 
         public Task<IEnumerable<string>> GetCustomColumnsForCustomerById(Guid customerId)
@@ -12,14 +37,30 @@
             throw new NotImplementedException();
         }
 
-        public Task<IEnumerable<IDictionary<string, object>>> GetDataAsDictionary(string databaseId, string schema, string tableName)
+        public async Task<(bool, string)> GenerateCSV(IEnumerable<object> results, string fileNameAndPath)
         {
-            throw new NotImplementedException();
-        }
+            var file = !string.IsNullOrEmpty(fileNameAndPath) ? fileNameAndPath : File.Create(@"C:\results.csv").ToString();
+            var configuration = new CsvConfiguration(CultureInfo.InvariantCulture)
+            {
+                HasHeaderRecord = true,
+            };
 
-        public Task<IEnumerable<object>> GetDataAsObjects(string databaseId, string schema, string tableName)
-        {
-            throw new NotImplementedException();
+            try
+            {
+                using (var writer = new StreamWriter(file))
+                {
+                    using (var csvWriter = new CsvWriter(writer, configuration))
+                    {
+                        csvWriter.WriteRecords(results);
+                        return (true, "Csv file created.");
+                    }
+                }
+            }
+            catch (Exception exception)
+            {
+
+                return (false, $"An exception was encountered: {exception.Message}");
+            }
         }
     }
 }
